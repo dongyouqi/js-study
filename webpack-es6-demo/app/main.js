853b4f05d5a5e0e029b6416f0a2ec0b99ae95482
qidongyou <qidongyou@outlook.com>
2017-06-09 21:29:05 +0800

    学习字符串

--- webpack-es6-demo/app/main.js
+++ webpack-es6-demo/app/main.js
@@ -7,6 +7,56 @@ document.write(p.say());
 Proxy.count = 2;
 Proxy.aaa = 1111;
 
+let s = "日";
+var length = s.length;
+
+console.log(length);
+
+var charAt = s.charAt(0);
+
+console.log(charAt);
+
+let charCodeAt = s.charCodeAt(0)
+
+console.log(charCodeAt);
+
+console.log('------');
+
+import Str from './Str.js';
+
+let mystr = new Str('hello');
+
+mystr.toArray();
+var first =  mystr.first();
+
+console.log(first);
+
+var last = mystr.last();
+
+console.log(last);
+
+let searchStr = mystr.search('12312');
+
+console.log(searchStr);
+
+let reg = /^.$/u.test('中');
+
+console.log(reg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
